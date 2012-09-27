e30aacdf6aaf1aa3c6dc9e754719d3cc57781b0b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-09-27 21:18:28 +0400

    Initial

---
+++ README.md
@@ -0,0 +1,28 @@
+The Bot
+=======
+
+This is a general purpose chat bot, extensible in varios ways.
+The reason, why it was written is because NodeJS and CoffeeScript are sucks and
+Hubot uses them.
+
+The Bot is written in orthodox Python and can be installed via [pip][].
+It's functionality can be extended by installation of additional python packages,
+you don't have to clone a repository and hack some code there.
+
+Alternatives
+------------
+
+There are some bots written in Python, but all of them sucks, because,
+most of the works only with IRC and none of them has such beautiful
+architecture as The Bot.
+
+But if you still have some doubts, here is a list of some bots. Go, try
+them and come back to ask The Bot to forgive you treason.
+
+* http://gozerbot.org/ and his son https://code.google.com/p/jsonbot/
+* http://pypi.python.org/pypi/supybot/
+* https://github.com/brunobord/cmdbot/
+* http://inamidst.com/phenny/ и его продолжение для Python3 https://github.com/sbp/duxlot/
+* https://github.com/toastdriven/toastbot использует irckit и gevent
+
+[pip]: http://pypi.python.org/pypi/pip
---
+++ tests.py
@@ -0,0 +1,84 @@
+from thebot import Bot, Request
+from nose.tools import eq_, assert_raises
+
+class TestAdapter(object):
+    def __init__(self, callback):
+        self.callback = callback
+        self._lines = []
+
+    def start(self):
+        pass
+
+    def write(self, input_line):
+        """This method is for test purpose.
+        """
+        lines = self._lines
+        class TestRequest(Request):
+            def respond(self, message):
+                lines.append(message)
+
+        self.callback(TestRequest(input_line))
+
+
+class TestPlugin(object):
+    def get_callbacks(self):
+        return [
+            ('^show me a cat$', self.show_a_cat),
+            ('^find (?P<this>.*)$', self.find),
+        ]
+
+    def show_a_cat(self, request, match):
+        request.respond('the Cat')
+
+    def find(self, request, match):
+        request.respond('I found {0}'.format(match.group('this')))
+
+
+def test_install_adapters():
+    bot = Bot(adapters=[TestAdapter], plugins=[])
+    assert len(bot.adapters) == 1
+
+
+def test_install_plugins():
+    bot = Bot(adapters=[], plugins=[TestPlugin])
+    assert len(bot.adapters) == 0
+    assert len(bot.plugins) == 1
+    assert len(bot.patterns) == 2
+
+
+def test_one_line():
+    bot = Bot(adapters=[TestAdapter], plugins=[TestPlugin])
+    adapter = bot.adapters[0]
+
+    eq_(adapter._lines, [])
+    adapter.write('show me a cat')
+    eq_(adapter._lines, ['the Cat'])
+
+    adapter.write('find Umputun')
+    eq_(adapter._lines[-1], 'I found Umputun')
+
+
+def test_unknown_command():
+    bot = Bot(adapters=[TestAdapter], plugins=[TestPlugin])
+    adapter = bot.adapters[0]
+
+    eq_(adapter._lines, [])
+    adapter.write('some command')
+    eq_(adapter._lines, ['I don\'t know command "some command".'])
+
+
+def test_exception_raised_if_plugin_returns_not_none():
+    class BadPlugin(object):
+        def get_callbacks(self):
+            return [
+                ('^do$', self.do),
+            ]
+
+        def do(self, request, match):
+            return 'Hello world'
+
+
+    bot = Bot(adapters=[TestAdapter], plugins=[BadPlugin])
+    adapter = bot.adapters[0]
+
+    assert_raises(RuntimeError, adapter.write, 'do')
