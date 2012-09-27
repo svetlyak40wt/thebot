8d4b8ddee9a357870b16d419d0e45aefebb0534c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-09-27 22:08:17 +0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thebot',
+    version='0.1.0',
+    description=(
+    ),
+    keywords='chat irc xmpp basecamp jira fun',
+    license = 'New BSD License',
+    author="Alexander Artemenko",
+    author_email='svetlyak.40wt@gmail.com',
+    url='http://github.com/svetlyak40wt/thebot/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Server',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    packages=find_packages(),
+    scripts=['scripts/thebot'],
+    install_requires = open('requirements.txt').readlines()
+)
