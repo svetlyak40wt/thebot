ad6af47961ceb0f95d55e1ba7fa658ad3dc184db
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-10-03 12:17:52 +0400

    Added class 'ThreadedPlugin' for plugins which needed a background job.

--- thebot/__init__.py
+++ thebot/__init__.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 
-import importlib
-import re
 import argparse
+import importlib
 import logging
+import re
 import shelve
+import threading
+import time
 
 # pass this object to callback, to terminate the bot
 EXIT = object()
@@ -41,6 +43,63 @@ class Plugin(object):
                 patterns = getattr(value, '_patterns', [])
                 for pattern in patterns:
                     yield (pattern, value)
+
+
+class ThreadedPlugin(Plugin):
+    """ThreadedPlugin allows you to do some processing in a background thread.
+
+    This class will take care on proper thread execution and termination.
+
+    * First, implement method `do_job`, which will be executed with given interval.
+    * Then run method `self.start_worker(interval=60)` to run a thread.
+      It will call your do_job callback each 60 seconds.
+    * To stop job execution, call `self.stop_worker()`.
+
+    See `thebot-instagram`, as an example.
+    """
+    def do_job(self):
+        raise NotImplemented('Implement "do_job" method to get real work done.')
+
+
+    def start_worker(self, interval=60):
+        thread = getattr(self, '_thread', None)
+        if thread is not None and thread.is_alive():
+            return
+
+        self._event = threading.Event()
+        self._thread = threading.Thread(target=self._worker, kwargs=dict(interval=interval))
+        self._thread.daemon = True
+        self._thread.start()
+
+    def stop_worker(self):
+        event = getattr(self, '_event', None)
+        if event is not None:
+            event.set()
+
+    def _worker(self, interval=60):
+        countdown = 0
+        logger = logging.getLogger('thebot.' + self.__class__.__name__)
+
+        on_start = getattr(self, 'on_start', None)
+        if on_start is not None:
+            on_start()
+
+        while not self._event.is_set():
+            if countdown == 0:
+                try:
+                    self.do_job()
+                except Exception:
+                    logger.exception('Error during the task execution')
+
+                countdown = interval
+            else:
+                countdown -= 1
+
+            time.sleep(1)
+
+        on_stop = getattr(self, 'on_stop', None)
+        if on_stop is not None:
+            on_stop()
 
 
 def route(pattern):
