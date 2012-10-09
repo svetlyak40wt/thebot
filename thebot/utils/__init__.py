b975ed918478212d87c1515b1c4c6ad58c321a02
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-10-09 09:11:02 +0400

    Fixes force_unicode for Python3.

--- thebot/utils/__init__.py
+++ thebot/utils/__init__.py
@@ -1,10 +1,15 @@
+import sys
 
-def force_unicode(value):
-    if isinstance(value, str):
-        value = value.decode('utf-8')
-    return value
+if sys.version_info.major == 3:
+    force_unicode = lambda x: x
+    force_str = lambda x: x
+else:
+    def force_unicode(value):
+        if isinstance(value, str):
+            value = value.decode('utf-8')
+        return value
 
-def force_str(value):
-    if isinstance(value, unicode):
-        value = value.encode('utf-8')
-    return value
+    def force_str(value):
+        if isinstance(value, str):
+            value = value.encode('utf-8')
+        return value
