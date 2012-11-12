537e373cb9004dd07d6092ab94fb4cd8087c26aa
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-11-12 09:27:29 +0000

    Fix for python3.

--- thebot/utils/__init__.py
+++ thebot/utils/__init__.py
@@ -2,6 +2,12 @@ from __future__ import absolute_import, unicode_literals
 
 import sys
 
+try:
+    from collections import MutableMapping
+except ImportError:
+    from UserDict import DictMixin as MutableMapping
+
+
 if sys.version_info.major == 3:
     force_unicode = lambda x: x
     force_str = lambda x: x
@@ -16,8 +22,16 @@ else:
             value = value.encode('utf-8')
         return value
 
-try:
-    from collections import MutableMapping
-except ImportError:
-    from UserDict import DictMixin as MutableMapping
+
+def printable(cls):
+    """This decorator creates __str__ object for python2.x
+    or renames __uncode__ into __str__ for python3.x
+    """
+    if sys.version_info.major == 3:
+        setattr(cls, '__str__', getattr(cls, '__unicode__'))
+        delattr(cls, '__unicode__')
+    else:
+        setattr(cls, '__str__', lambda self: self.__unicode__().encode('utf8'))
+
+    return cls
 
